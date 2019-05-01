de88a784f4a4ab433ab30c3d8a8013990006efef
f76map <49995976+f76map@users.noreply.github.com>
2019-05-01 22:40:47 +0300

    Create NutryanoyGrib.js

---
+++ scripts/Rasteniya/NutryanoyGrib.js
@@ -0,0 +1,11 @@
+	var nutryanoygribicons = L.layerGroup().addTo(map);
+	function NutryanoyGribOnOff() {
+	var chbox;
+	chbox=document.getElementById('NutryanoyGribButton');
+	if (chbox.checked) {
+	L.circleMarker([-3956, 157951], {color: "#B22222", fillColor: "#000000", fillOpacity: "0.8"}).addTo(nutryanoygribicons).bindPopup("Нутряной гриб - 10 шт.<br>[Внутри локации Норы]").bindTooltip("Нутряной гриб - 10 шт.<br>[Внутри локации Норы]", {direction: 'top'});
+	}
+	else {
+		nutryanoygribicons.clearLayers();
+	}
+}
