9d0a5d3322e549c95b03245b33cd75b4b3bbf1d4
f76map <49995976+f76map@users.noreply.github.com>
2019-05-01 22:39:52 +0300

    Create Arbuz.js

---
+++ scripts/Rasteniya/Arbuz.js
@@ -0,0 +1,15 @@
+	var arbuzicons = L.layerGroup().addTo(map);
+	function ArbuzOnOff() {
+	var chbox;
+	chbox=document.getElementById('ArbuzButton');
+	if (chbox.checked) {
+	L.circleMarker([-59735, 117369], {color: "#9ACD32"}).addTo(arbuzicons).bindPopup("Арбуз").bindTooltip("Арбуз", {direction: 'top'});
+
+	L.circleMarker([-41518, -175531], {color: "#9ACD32"}).addTo(arbuzicons).bindPopup("Арбуз").bindTooltip("Арбуз", {direction: 'top'});
+	}
+	else {
+		arbuzicons.clearLayers();
+	}
+}
+//1 в Убежище 94
+//4 в лаборатории
