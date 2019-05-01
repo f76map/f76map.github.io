3b53274daa4d752e334cd0fb2c301340bc1a1111
f76map <49995976+f76map@users.noreply.github.com>
2019-05-01 22:40:17 +0300

    Create Lihocvet.js

---
+++ scripts/Rasteniya/Lihocvet.js
@@ -0,0 +1,11 @@
+	var lihocveticons = L.layerGroup().addTo(map);
+	function LihocvetOnOff() {
+	var chbox;
+	chbox=document.getElementById('LihocvetButton');
+	if (chbox.checked) {
+	L.circleMarker([71179, -172735], {color: "#20B2AA"}).addTo(lihocveticons).bindPopup("Лихоцвет").bindTooltip("Лихоцвет");
+	}
+	else {
+		lihocveticons.clearLayers();
+	}
+}
