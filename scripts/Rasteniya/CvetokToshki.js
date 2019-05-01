203932c1a160c22fc7b0a9b745aa4a8d0c400bba
f76map <49995976+f76map@users.noreply.github.com>
2019-05-01 22:40:09 +0300

    Create CvetokToshki.js

---
+++ scripts/Rasteniya/CvetokToshki.js
@@ -0,0 +1,65 @@
+	var cvetoktoshkiicons = L.layerGroup().addTo(map);
+	function CvetokToshkiOnOff() {
+	var chbox;
+	chbox=document.getElementById('CvetokToshkiButton');
+	if (chbox.checked) {
+	L.circleMarker([-43899, -90757], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-43734, -91099], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-43648, -90930], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-19603, -126162], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-19549, -126149], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-19138, -126099], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-19313, -125905], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-21671, -122398], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-21834, -122220], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-21696, -122656], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-54203, -129511], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-54288, -129644], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-53053, -128443], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-32955, -111297], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-34236, -111418], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-32835, -111258], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-32799, -110211], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-34215, -110226], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-33997, -110089], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([107222, -22416], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([107765, -23573], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([29671, -21161], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([29844, -21307], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([29680, -21284], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-8500, -157562], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-8602, -157679], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-8580, -157884], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-8682, -158001], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-8382, -157814], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-8264, -157673], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-8261, -157509], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-8145, -157407], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-7551, -157084], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-7715, -157203], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-7687, -157356], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-7839, -157475], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-7325, -157179], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-7472, -157310], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-7313, -157005], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-7177, -156903], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([137297, 144673], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([137118, 144368], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([137309, 144493], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([137218, 144478], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([136975, 144707], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([136946, 144719], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([137326, 144674], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([137403, 144354], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([137854, 144588], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([137590, 144869], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([137958, 144789], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([137657, 144558], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([137596, 144553], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([137967, 144503], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	L.circleMarker([-60046, 117171], {color: "#991199"}).addTo(cvetoktoshkiicons).bindPopup("Цветок дикой тошки").bindTooltip("Цветок дикой тошки", {direction: 'top'});
+	}
+	else {
+		cvetoktoshkiicons.clearLayers();
+	}
+}
