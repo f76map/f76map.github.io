27405867191e5196d13cf64480b26071af41ac88
f76map <49995976+f76map@users.noreply.github.com>
2019-09-18 13:29:19 +0300

    Create url-hash.js

---
+++ scripts/url-hash.js
@@ -0,0 +1,255 @@
+function urlhash() {
+if(window.location.hash == '#treasure') {
+		TreasureButton.checked = 1;
+		TreasureOnOff();
+    }
+if(window.location.hash == '#magazine') {
+		MagazineButton.checked = 1;
+		MagazineOnOff();
+    }
+if(window.location.hash == '#bobblehead') {
+		BobbleheadButton.checked = 1;
+		BobbleheadOnOff();
+    }
+if(window.location.hash == '#capstash') {
+		CapStashButton.checked = 1;
+		CapStashOnOff();
+    }
+// Случайные встречи
+if(window.location.hash == '#assault') {
+		REAssaultButton.checked = 1;
+		REAssaultOnOff();
+    }
+if(window.location.hash == '#object') {
+		REObjectButton.checked = 1;
+		REObjectOnOff();
+    }
+if(window.location.hash == '#scene') {
+		RESceneButton.checked = 1;
+		RESceneOnOff();
+    }
+if(window.location.hash == '#travel') {
+		RETravelButton.checked = 1;
+		RETravelOnOff();
+    }
+// Мобы
+if(window.location.hash == '#strangemobs') {
+		StrangeMobsButton.checked = 1;
+		StrangeMobsOnOff();
+    }
+// Месторождения для экстракторов
+if(window.location.hash == '#medy') {
+		MedyButton.checked = 1;
+		MedyOnOff();
+    }
+if(window.location.hash == '#hlam') {
+		HlamButton.checked = 1;
+		HlamOnOff();
+    }
+if(window.location.hash == '#ugoly') {
+		UgolyButton.checked = 1;
+		UgolyOnOff();
+    }
+if(window.location.hash == '#beton') {
+		BetonButton.checked = 1;
+		BetonOnOff();
+    }
+if(window.location.hash == '#maslo') {
+		MasloButton.checked = 1;
+		MasloOnOff();
+    }
+if(window.location.hash == '#zoloto') {
+		ZolotoButton.checked = 1;
+		ZolotoOnOff();
+    }
+if(window.location.hash == '#jelezo') {
+		JelezoButton.checked = 1;
+		JelezoOnOff();
+    }
+if(window.location.hash == '#svinec') {
+		SvinecButton.checked = 1;
+		SvinecOnOff();
+    }
+if(window.location.hash == '#serebro') {
+		SerebroButton.checked = 1;
+		SerebroOnOff();
+    }
+if(window.location.hash == '#kislota') {
+		KislotaButton.checked = 1;
+		KislotaOnOff();
+    }
+if(window.location.hash == '#udobrenie') {
+		UdobrenieButton.checked = 1;
+		UdobrenieOnOff();
+    }
+if(window.location.hash == '#aluminiy') {
+		AluminiyButton.checked = 1;
+		AluminiyOnOff();
+    }
+if(window.location.hash == '#kristalli') {
+		KristalliButton.checked = 1;
+		KristalliOnOff();
+    }
+if(window.location.hash == '#chorniytitan') {
+		ChorniyTitanButton.checked = 1;
+		ChorniyTitanOnOff();
+    }
+if(window.location.hash == '#yaderniymaterial') {
+		YaderniyMaterialButton.checked = 1;
+		YaderniyMaterialOnOff();
+    }
+// Растения
+if(window.location.hash == '#grib') {
+		GribButton.checked = 1;
+		GribOnOff();
+    }
+if(window.location.hash == '#arbuz') {
+		ArbuzButton.checked = 1;
+		ArbuzOnOff();
+    }
+if(window.location.hash == '#astra') {
+		AstraButton.checked = 1;
+		AstraOnOff();
+    }
+if(window.location.hash == '#toshka') {
+		ToshkaButton.checked = 1;
+		ToshkaOnOff();
+    }
+if(window.location.hash == '#tyikva') {
+		TyikvaButton.checked = 1;
+		TyikvaOnOff();
+    }
+if(window.location.hash == '#klukva') {
+		KlukvaButton.checked = 1;
+		KlukvaOnOff();
+    }
+if(window.location.hash == '#morkovy') {
+		MorkovyButton.checked = 1;
+		MorkovyOnOff();
+    }
+if(window.location.hash == '#ejevika') {
+		EjevikaButton.checked = 1;
+		EjevikaOnOff();
+    }
+if(window.location.hash == '#kukuruza') {
+		KukuruzaButton.checked = 1;
+		KukuruzaOnOff();
+    }
+if(window.location.hash == '#lihocvet') {
+		LihocvetButton.checked = 1;
+		LihocvetOnOff();
+    }
+if(window.location.hash == '#ilofasoly') {
+		IlofasolyButton.checked = 1;
+		IlofasolyOnOff();
+    }
+if(window.location.hash == '#krovolist') {
+		KrovolistButton.checked = 1;
+		KrovolistOnOff();
+    }
+if(window.location.hash == '#mozgogrib') {
+		MozgogribButton.checked = 1;
+		MozgogribOnOff();
+    }
+if(window.location.hash == '#mutafrukt') {
+		MutafruktButton.checked = 1;
+		MutafruktOnOff();
+    }
+if(window.location.hash == '#peplocvet') {
+		PeplocvetButton.checked = 1;
+		PeplocvetOnOff();
+    }
+if(window.location.hash == '#smolyanika') {
+		SmolyanikaButton.checked = 1;
+		SmolyanikaOnOff();
+    }
+if(window.location.hash == '#hvostolom') {
+		HvostolomButton.checked = 1;
+		HvostolomOnOff();
+    }
+if(window.location.hash == '#britvozlak') {
+		BritvozlakButton.checked = 1;
+		BritvozlakOnOff();
+    }
+if(window.location.hash == '#gryaznocvet') {
+		GryaznocvetButton.checked = 1;
+		GryaznocvetOnOff();
+    }
+if(window.location.hash == '#rododendron') {
+		RododendronButton.checked = 1;
+		RododendronOnOff();
+    }
+if(window.location.hash == '#chertopoloh') {
+		ChertopolohButton.checked = 1;
+		ChertopolohOnOff();
+    }
+if(window.location.hash == '#nochnoypolzun') {
+		NochnoyPolzunButton.checked = 1;
+		NochnoyPolzunOnOff();
+    }
+if(window.location.hash == '#nutryanoygrib') {
+		NutryanoyGribButton.checked = 1;
+		NutryanoyGribOnOff();
+    }
+if(window.location.hash == '#ognenniygrib') {
+		OgnenniyGribButton.checked = 1;
+		OgnenniyGribOnOff();
+    }
+if(window.location.hash == '#yaycomolerota') {
+		YaycoMolerotaButton.checked = 1;
+		YaycoMolerotaOnOff();
+    }
+if(window.location.hash == '#pepelynayaroza') {
+		PepelynayaRozaButton.checked = 1;
+		PepelynayaRozaOnOff();
+    }
+if(window.location.hash == '#yagodahlopushka') {
+		YagodaHlopushkaButton.checked = 1;
+		YagodaHlopushkaOnOff();
+    }
+if(window.location.hash == '#korenyjenyshenya') {
+		KorenyJenyshenyaButton.checked = 1;
+		KorenyJenyshenyaOnOff();
+    }
+if(window.location.hash == '#svetgrib') {
+		SvetGribButton.checked = 1;
+		SvetGribOnOff();
+    }
+if(window.location.hash == '#cvetokdushitelya') {
+		CvetokDushitelyaButton.checked = 1;
+		CvetokDushitelyaOnOff();
+    }
+if(window.location.hash == '#svetsmola') {
+		SvetSmolaButton.checked = 1;
+		SvetSmolaOnOff();
+    }
+if(window.location.hash == '#zarajklukva') {
+		ZarajKlukvaButton.checked = 1;
+		ZarajKlukvaOnOff();
+    }
+if(window.location.hash == '#paporotnik') {
+		PaporotnikButton.checked = 1;
+		PaporotnikOnOff();
+    }
+if(window.location.hash == '#kizil') {
+		KizilButton.checked = 1;
+		KizilOnOff();
+    }
+if(window.location.hash == '#cvetoktoshki') {
+		CvetokToshkiButton.checked = 1;
+		CvetokToshkiOnOff();
+    }
+if(window.location.hash == '#cvetoktyikvyi') {
+		CvetokTyikvyiButton.checked = 1;
+		CvetokTyikvyiOnOff();
+    }
+if(window.location.hash == '#cvetokarbuza') {
+		CvetokArbuzaButton.checked = 1;
+		CvetokArbuzaOnOff();
+    }
+if(window.location.hash == '#cvetokmorkovi') {
+		CvetokMorkoviButton.checked = 1;
+		CvetokMorkoviOnOff();
+    }
+	history.pushState("", document.title, window.location.pathname + window.location.search);
+}
