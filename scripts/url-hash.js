d9fa9c5784cce955949ad2064dfea9ab6405de60
f76map <49995976+f76map@users.noreply.github.com>
2019-09-20 23:23:05 +0300

    Update url-hash.js

--- scripts/url-hash.js
+++ scripts/url-hash.js
@@ -251,5 +251,631 @@ if(window.location.hash == '#cvetokmorkovi') {
 		CvetokMorkoviButton.checked = 1;
 		CvetokMorkoviOnOff();
     }
+	
+// -------------------- Без включения локаций --------------------
+if(window.location.hash == '#treasure_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		TreasureButton.checked = 1;
+		TreasureOnOff();
+    }
+if(window.location.hash == '#magazine_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		MagazineButton.checked = 1;
+		MagazineOnOff();
+    }
+if(window.location.hash == '#bobblehead_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		BobbleheadButton.checked = 1;
+		BobbleheadOnOff();
+    }
+if(window.location.hash == '#capstash_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		CapStashButton.checked = 1;
+		CapStashOnOff();
+    }
+// Случайные встречи (без локаций)
+if(window.location.hash == '#assault_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		REAssaultButton.checked = 1;
+		REAssaultOnOff();
+    }
+if(window.location.hash == '#object_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		REObjectButton.checked = 1;
+		REObjectOnOff();
+    }
+if(window.location.hash == '#scene_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		RESceneButton.checked = 1;
+		RESceneOnOff();
+    }
+if(window.location.hash == '#travel_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		RETravelButton.checked = 1;
+		RETravelOnOff();
+    }
+// Мобы
+if(window.location.hash == '#strangemobs_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		StrangeMobsButton.checked = 1;
+		StrangeMobsOnOff();
+    }
+// Месторождения для экстракторов (без локаций)
+if(window.location.hash == '#medy_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		MedyButton.checked = 1;
+		MedyOnOff();
+    }
+if(window.location.hash == '#hlam_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		HlamButton.checked = 1;
+		HlamOnOff();
+    }
+if(window.location.hash == '#ugoly_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		UgolyButton.checked = 1;
+		UgolyOnOff();
+    }
+if(window.location.hash == '#beton_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		BetonButton.checked = 1;
+		BetonOnOff();
+    }
+if(window.location.hash == '#maslo_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		MasloButton.checked = 1;
+		MasloOnOff();
+    }
+if(window.location.hash == '#zoloto_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		ZolotoButton.checked = 1;
+		ZolotoOnOff();
+    }
+if(window.location.hash == '#jelezo_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		JelezoButton.checked = 1;
+		JelezoOnOff();
+    }
+if(window.location.hash == '#svinec_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		SvinecButton.checked = 1;
+		SvinecOnOff();
+    }
+if(window.location.hash == '#serebro_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		SerebroButton.checked = 1;
+		SerebroOnOff();
+    }
+if(window.location.hash == '#kislota_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		KislotaButton.checked = 1;
+		KislotaOnOff();
+    }
+if(window.location.hash == '#udobrenie_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		UdobrenieButton.checked = 1;
+		UdobrenieOnOff();
+    }
+if(window.location.hash == '#aluminiy_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		AluminiyButton.checked = 1;
+		AluminiyOnOff();
+    }
+if(window.location.hash == '#kristalli_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		KristalliButton.checked = 1;
+		KristalliOnOff();
+    }
+if(window.location.hash == '#chorniytitan_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		ChorniyTitanButton.checked = 1;
+		ChorniyTitanOnOff();
+    }
+if(window.location.hash == '#yaderniymaterial_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		YaderniyMaterialButton.checked = 1;
+		YaderniyMaterialOnOff();
+    }
+// Растения (без локаций)
+if(window.location.hash == '#grib_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		GribButton.checked = 1;
+		GribOnOff();
+    }
+if(window.location.hash == '#arbuz_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		ArbuzButton.checked = 1;
+		ArbuzOnOff();
+    }
+if(window.location.hash == '#astra_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		AstraButton.checked = 1;
+		AstraOnOff();
+    }
+if(window.location.hash == '#toshka_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		ToshkaButton.checked = 1;
+		ToshkaOnOff();
+    }
+if(window.location.hash == '#tyikva_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		TyikvaButton.checked = 1;
+		TyikvaOnOff();
+    }
+if(window.location.hash == '#klukva_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		KlukvaButton.checked = 1;
+		KlukvaOnOff();
+    }
+if(window.location.hash == '#morkovy_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		MorkovyButton.checked = 1;
+		MorkovyOnOff();
+    }
+if(window.location.hash == '#ejevika_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		EjevikaButton.checked = 1;
+		EjevikaOnOff();
+    }
+if(window.location.hash == '#kukuruza_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		KukuruzaButton.checked = 1;
+		KukuruzaOnOff();
+    }
+if(window.location.hash == '#lihocvet_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		LihocvetButton.checked = 1;
+		LihocvetOnOff();
+    }
+if(window.location.hash == '#ilofasoly_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		IlofasolyButton.checked = 1;
+		IlofasolyOnOff();
+    }
+if(window.location.hash == '#krovolist_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		KrovolistButton.checked = 1;
+		KrovolistOnOff();
+    }
+if(window.location.hash == '#mozgogrib_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		MozgogribButton.checked = 1;
+		MozgogribOnOff();
+    }
+if(window.location.hash == '#mutafrukt_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		MutafruktButton.checked = 1;
+		MutafruktOnOff();
+    }
+if(window.location.hash == '#peplocvet_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		PeplocvetButton.checked = 1;
+		PeplocvetOnOff();
+    }
+if(window.location.hash == '#smolyanika_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		SmolyanikaButton.checked = 1;
+		SmolyanikaOnOff();
+    }
+if(window.location.hash == '#hvostolom_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		HvostolomButton.checked = 1;
+		HvostolomOnOff();
+    }
+if(window.location.hash == '#britvozlak_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		BritvozlakButton.checked = 1;
+		BritvozlakOnOff();
+    }
+if(window.location.hash == '#gryaznocvet_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		GryaznocvetButton.checked = 1;
+		GryaznocvetOnOff();
+    }
+if(window.location.hash == '#rododendron_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		RododendronButton.checked = 1;
+		RododendronOnOff();
+    }
+if(window.location.hash == '#chertopoloh_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		ChertopolohButton.checked = 1;
+		ChertopolohOnOff();
+    }
+if(window.location.hash == '#nochnoypolzun_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		NochnoyPolzunButton.checked = 1;
+		NochnoyPolzunOnOff();
+    }
+if(window.location.hash == '#nutryanoygrib_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		NutryanoyGribButton.checked = 1;
+		NutryanoyGribOnOff();
+    }
+if(window.location.hash == '#ognenniygrib_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		OgnenniyGribButton.checked = 1;
+		OgnenniyGribOnOff();
+    }
+if(window.location.hash == '#yaycomolerota_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		YaycoMolerotaButton.checked = 1;
+		YaycoMolerotaOnOff();
+    }
+if(window.location.hash == '#pepelynayaroza_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		PepelynayaRozaButton.checked = 1;
+		PepelynayaRozaOnOff();
+    }
+if(window.location.hash == '#yagodahlopushka_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		YagodaHlopushkaButton.checked = 1;
+		YagodaHlopushkaOnOff();
+    }
+if(window.location.hash == '#korenyjenyshenya_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		KorenyJenyshenyaButton.checked = 1;
+		KorenyJenyshenyaOnOff();
+    }
+if(window.location.hash == '#svetgrib_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		SvetGribButton.checked = 1;
+		SvetGribOnOff();
+    }
+if(window.location.hash == '#cvetokdushitelya_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		CvetokDushitelyaButton.checked = 1;
+		CvetokDushitelyaOnOff();
+    }
+if(window.location.hash == '#svetsmola_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		SvetSmolaButton.checked = 1;
+		SvetSmolaOnOff();
+    }
+if(window.location.hash == '#zarajklukva_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		ZarajKlukvaButton.checked = 1;
+		ZarajKlukvaOnOff();
+    }
+if(window.location.hash == '#paporotnik_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		PaporotnikButton.checked = 1;
+		PaporotnikOnOff();
+    }
+if(window.location.hash == '#kizil_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		KizilButton.checked = 1;
+		KizilOnOff();
+    }
+if(window.location.hash == '#cvetoktoshki_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		CvetokToshkiButton.checked = 1;
+		CvetokToshkiOnOff();
+    }
+if(window.location.hash == '#cvetoktyikvyi_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		CvetokTyikvyiButton.checked = 1;
+		CvetokTyikvyiOnOff();
+    }
+if(window.location.hash == '#cvetokarbuza_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		CvetokArbuzaButton.checked = 1;
+		CvetokArbuzaOnOff();
+    }
+if(window.location.hash == '#cvetokmorkovi_0') {
+		allicons.clearLayers();
+		LocationsButton.checked = 0;
+		map.removeControl( controlSearch );
+		Position.remove();
+		map.removeControl(map.zoomControl);
+		sidebar0.style.display = 'none';
+		CvetokMorkoviButton.checked = 1;
+		CvetokMorkoviOnOff();
+    }
 	history.pushState("", document.title, window.location.pathname + window.location.search);
 }
