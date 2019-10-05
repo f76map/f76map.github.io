e502dae6bcfe3d2939404ed546885227e3bfa201
f76map <49995976+f76map@users.noreply.github.com>
2019-10-05 12:41:19 +0300

    Create AluminiyButton.js

---
+++ scripts/Objects/Mestorojdeniya/AluminiyButton.js
@@ -0,0 +1,43 @@
+	var aluminiyicons = L.layerGroup().addTo(map);
+	function AluminiyOnOff() {
+	var chbox;
+	chbox=document.getElementById('AluminiyButton');
+	if (chbox.checked) {
+	icons = new L.marker([137488, -156909], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([31544, -140672], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-159360, -152355], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-45375, -126394], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-73530, -125879], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-116079, -121651], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-156506, -95505], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-83785, -103376], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([197291, -9702], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-67060, 145513], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-74557, 135245], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([51315, 160631], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([71193, 172117], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([170472, 199374], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-169356, 176621], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-178712, 179717], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-127563, 125194], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-132667, 174101], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([197259, 124930], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-169411, -42593], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([142485, 60358], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([193835, 150128], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([119431, 130668], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([178450, 109260], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-80513, 19509], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([30318, 62441], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-62198, -127397], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([146993, 135288], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([150542, 142202], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([152498, 135482], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-87933, -34164], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([-92704, -2910], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	icons = new L.marker([22088, 122296], {icon: icon_aluminiy}).addTo(aluminiyicons).bindPopup("Месторождение алюминия").bindTooltip("Месторождение алюминия", {direction: 'top'});
+	}
+	else {
+		aluminiyicons.clearLayers();
+	}
+}
