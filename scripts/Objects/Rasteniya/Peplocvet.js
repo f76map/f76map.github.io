710c99563fc892f6d6568b6299f4548d4dca27da
f76map <49995976+f76map@users.noreply.github.com>
2019-10-18 22:59:54 +0300

    Update Peplocvet.js

--- scripts/Objects/Rasteniya/Peplocvet.js
+++ scripts/Objects/Rasteniya/Peplocvet.js
@@ -1,14 +1,22 @@
-	var peplocveticons = L.layerGroup().addTo(map);
-	function PeplocvetOnOff() {
+var peplocveticons = L.layerGroup().addTo(map);
+function PeplocvetOnOff() {
 	var chbox;
 	chbox=document.getElementById('PeplocvetButton');
 	if (chbox.checked) {
-	L.circleMarker([170257, -120254], {color: "#6495ED"}).addTo(peplocveticons).bindPopup("Пеплоцвет").bindTooltip("Пеплоцвет", {direction: 'top'});
-	L.circleMarker([170170, -121039], {color: "#6495ED"}).addTo(peplocveticons).bindPopup("Пеплоцвет").bindTooltip("Пеплоцвет", {direction: 'top'});
-	L.circleMarker([171153, -120486], {color: "#6495ED"}).addTo(peplocveticons).bindPopup("Пеплоцвет").bindTooltip("Пеплоцвет", {direction: 'top'});
-	L.circleMarker([172747, -119570], {color: "#6495ED"}).addTo(peplocveticons).bindPopup("Пеплоцвет").bindTooltip("Пеплоцвет", {direction: 'top'});
-	L.circleMarker([171229, -118646], {color: "#6495ED"}).addTo(peplocveticons).bindPopup("Пеплоцвет").bindTooltip("Пеплоцвет", {direction: 'top'});
-	L.circleMarker([172063, -118713], {color: "#6495ED"}).addTo(peplocveticons).bindPopup("Пеплоцвет").bindTooltip("Пеплоцвет", {direction: 'top'});
+		var peplocvet = [
+			[170257, -120254],
+			[170170, -121039],
+			[171153, -120486],
+			[172747, -119570],
+			[171229, -118646],
+			[172063, -118713]
+		];
+		for (var i = 0; i < peplocvet.length; i++) {
+			peplocvet_1 = new L.circleMarker([peplocvet[i][0],peplocvet[i][1]], {color: "#6495ED"})
+			.bindPopup("Пеплоцвет")
+			.bindTooltip("Пеплоцвет", {direction: 'top'})
+			.addTo(peplocveticons);
+		}
 	}
 	else {
 		peplocveticons.clearLayers();
