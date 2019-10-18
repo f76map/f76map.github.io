e8c5aaf29966d7bf4580014e42a03413251e25b1
f76map <49995976+f76map@users.noreply.github.com>
2019-10-18 23:00:21 +0300

    Update YaycoMolerota.js

--- scripts/Objects/Rasteniya/YaycoMolerota.js
+++ scripts/Objects/Rasteniya/YaycoMolerota.js
@@ -1,36 +1,44 @@
-	var yaycomolerotaicons = L.layerGroup().addTo(map);
-	function YaycoMolerotaOnOff() {
+var yaycomolerotaicons = L.layerGroup().addTo(map);
+function YaycoMolerotaOnOff() {
 	var chbox;
 	chbox=document.getElementById('YaycoMolerotaButton');
 	if (chbox.checked) {
-	L.circleMarker([72355, -180753], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72414, -180832], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72477, -180853], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72540, -180812], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72527, -180710], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72430, -180683], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72744, -177259], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72785, -177320], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73480, -177327], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73449, -177327], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73485, -177283], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73513, -177225], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73379, -177565], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73458, -177517], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73485, -177537], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73424, -177559], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72589, -177353], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72588, -177317], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72554, -177184], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72529, -177210], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72531, -177124], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73029, -177477], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72983, -177449], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([72981, -177419], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73093, -177483], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73160, -177489], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73192, -177469], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
-	L.circleMarker([73221, -177427], {color: "#FF0000"}).addTo(yaycomolerotaicons).bindPopup("Яйцо молерота").bindTooltip("Яйцо молерота", {direction: 'top'});
+		var yaycomolerota = [
+			[72355, -180753],
+			[72414, -180832],
+			[72477, -180853],
+			[72540, -180812],
+			[72527, -180710],
+			[72430, -180683],
+			[72744, -177259],
+			[72785, -177320],
+			[73480, -177327],
+			[73449, -177327],
+			[73485, -177283],
+			[73513, -177225],
+			[73379, -177565],
+			[73458, -177517],
+			[73485, -177537],
+			[73424, -177559],
+			[72589, -177353],
+			[72588, -177317],
+			[72554, -177184],
+			[72529, -177210],
+			[72531, -177124],
+			[73029, -177477],
+			[72983, -177449],
+			[72981, -177419],
+			[73093, -177483],
+			[73160, -177489],
+			[73192, -177469],
+			[73221, -177427]
+		];
+		for (var i = 0; i < yaycomolerota.length; i++) {
+			yaycomolerota_1 = new L.circleMarker([yaycomolerota[i][0],yaycomolerota[i][1]], {color: "#FF0000"})
+			.bindPopup("Яйцо молерота")
+			.bindTooltip("Яйцо молерота", {direction: 'top'})
+			.addTo(yaycomolerotaicons);
+		}
 	}
 	else {
 		yaycomolerotaicons.clearLayers();
