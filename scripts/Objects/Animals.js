3cedde15671bbf248e3f96c9b02914e3c5f62fcd
f76map <49995976+f76map@users.noreply.github.com>
2020-05-02 23:32:54 +0300

    Create Animals.js

---
+++ scripts/Objects/Animals.js
@@ -0,0 +1,28 @@
+$('<label class="switch" style="position: absolute; top: 100px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test1OnOff()" id="Test1Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 125px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test2OnOff()" id="Test2Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+//Жевательная резинка
+$('<label class="switch" style="position: absolute; top: 150px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test3OnOff()" id="Test3Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+//Места спавна животных (Бобер, Брамин, Кошка, Курица, Светлячок, Лисица, Опоссум, Совенок, Кролик, Лягушка, Рад-олень, Белка)
+//LvlCritterA
+$('<label class="switch" style="position: absolute; top: 175px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test4OnOff()" id="Test4Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+//LvlCritterB
+$('<label class="switch" style="position: absolute; top: 200px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test5OnOff()" id="Test5Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+
+$('<label class="switch" style="position: absolute; top: 250px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test6OnOff()" id="Test6Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 275px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test7OnOff()" id="Test7Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 300px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test8OnOff()" id="Test8Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 325px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test9OnOff()" id="Test9Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 350px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test10OnOff()" id="Test10Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+
+$('<label class="switch" style="position: absolute; top: 400px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test11OnOff()" id="Test11Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 425px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test12OnOff()" id="Test12Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 450px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test13OnOff()" id="Test13Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 475px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test14OnOff()" id="Test14Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 500px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test15OnOff()" id="Test15Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+
+$('<label class="switch" style="position: absolute; top: 550px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test16OnOff()" id="Test16Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 575px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test17OnOff()" id="Test17Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 600px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test18OnOff()" id="Test18Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 625px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test19OnOff()" id="Test19Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+$('<label class="switch" style="position: absolute; top: 650px; right: 10px; z-index: 400;"><input type="checkbox" onchange="Test20OnOff()" id="Test20Button" autocomplete="off"><span class="slider"></span></label>').appendTo('body');
+
