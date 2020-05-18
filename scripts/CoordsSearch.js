faacc1d31ee07430e7b7caaa9da29edfdd6b9670
f76map <49995976+f76map@users.noreply.github.com>
2020-05-18 15:23:08 +0300

    Create CoordsSearch.js

---
+++ scripts/CoordsSearch.js
@@ -0,0 +1,30 @@
+let manymarkersicons = L.layerGroup().addTo(map);
+function MarkersSet() {
+manymarkersicons.clearLayers();
+	let mm = document.getElementById('t01').value;
+	mm = mm.replace(/\s/g, '');
+	mm = mm.replace(/(\],\[)/g, '] , [');
+	mm = mm.split(' , ');
+	for (let i = 0; i < mm.length; i++) {
+		mm[i] = mm[i].replace('[', '');
+		mm[i] = mm[i].replace(']', '');
+		mm[i] = mm[i].split(',');
+	}
+	for (let i = 0; i < mm.length; i++) {
+		if ($("input[name=coords_icon]:checked").val() == 1) {
+			new L.circleMarker([mm[i][0],mm[i][1]], {pane: "top", color: "#ffff00"})
+			.bindPopup("X: "+mm[i][1]+"<br>Y: "+mm[i][0])
+			.bindTooltip("X: "+mm[i][1]+"<br>Y: "+mm[i][0], {direction: 'top'})
+			.addTo(manymarkersicons);
+		}
+		if ($("input[name=coords_icon]:checked").val() == 2) {
+			new L.marker([mm[i][0],mm[i][1]])
+			.bindPopup("X: "+mm[i][1]+"<br>Y: "+mm[i][0])
+			.bindTooltip("X: "+mm[i][1]+"<br>Y: "+mm[i][0], {direction: 'top'})
+			.addTo(manymarkersicons);
+		}
+	}
+}
+function MarkersDel() {manymarkersicons.clearLayers();}
+function coords_box_show() {$('#coords_fullbox').show();}
+function coords_box_hide() {$('#coords_fullbox').hide();}
